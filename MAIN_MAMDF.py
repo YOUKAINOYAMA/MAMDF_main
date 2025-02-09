dcfaa6f6ae5507dc370592e2045ac17caf9ee466
YOUKAINOYAMA <104356705+YOUKAINOYAMA@users.noreply.github.com>
2025-02-09 17:12:18 +0800

    Update MAIN_MAMDF.py

--- MAIN_MAMDF.py
+++ MAIN_MAMDF.py
@@ -5,8 +5,6 @@ from torch.nn import functional as F
 from torchvision import datasets, models
 
 
-
-
 def get_cnn_model():
     cnn_model = models.resnet50(pretrained=True)
     num_ftrs = cnn_model.fc.in_features
@@ -15,58 +13,6 @@ def get_cnn_model():
     cnn_model.fc = nn.Linear(num_ftrs, 100)
     # cnn_model(b_img.to(device)).shape
     return cnn_model
-
-
-# Global Filter
-class FeedForward(nn.Module):
-    def __init__(self, dim, hidden_dim, dropout=0.):
-        super().__init__()
-        self.net = nn.Sequential(
-            nn.Linear(dim, hidden_dim),
-            nn.GELU(),
-            nn.Dropout(dropout),
-            nn.Linear(hidden_dim, dim),
-            nn.Dropout(dropout)
-        )
-
-    def forward(self, x):
-        return self.net(x)
-
-
-class PreNorm(nn.Module):
-    def __init__(self, dim, fn):
-        super().__init__()
-        self.norm = nn.LayerNorm(dim)
-        self.fn = fn
-
-    def forward(self, x, **kwargs):
-        return self.fn(self.norm(x), **kwargs)
-
-
-class FNetBlock(nn.Module):
-    def __init__(self):
-        super().__init__()
-
-    def forward(self, x):
-        x = torch.fft.fft(torch.fft.fft(x, dim=-1), dim=-2).real
-        return x
-
-
-class FNet(nn.Module):
-    def __init__(self, dim, depth, mlp_dim, dropout=0.):
-        super().__init__()
-        self.layers = nn.ModuleList([])
-        for _ in range(depth):
-            self.layers.append(nn.ModuleList([
-                PreNorm(dim, FNetBlock()),
-                PreNorm(dim, FeedForward(dim, mlp_dim, dropout=dropout))
-            ]))
-
-    def forward(self, x):
-        for attn, ff in self.layers:
-            x = attn(x) + x
-            x = ff(x) + x
-        return x
 
 
 class cross_attention_block(nn.Module):
